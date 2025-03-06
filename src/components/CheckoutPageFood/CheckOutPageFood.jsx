4abedf6621b9d4c634bf11d59f79dfbbbf81a69d
Nishadchowdhury <Nishadhj111@gmail.com>
2025-03-06 11:03:01 +0600

    checkout page image loading problem solved

--- src/components/CheckoutPageFood/CheckOutPageFood.jsx
+++ src/components/CheckoutPageFood/CheckOutPageFood.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 import { TbCurrencyTaka } from "react-icons/tb"
+import { useParams } from "react-router-dom"
 
 function CheckOutPageFood({ cuisineImg, availableAt, name, price, quantity, setError }) {
 
     const [show, setShow] = useState(true)
-console.log(cuisineImg);
+    const params = useParams()
+
+    console.log(params);
+
     function imgError() {
         setShow(false)
         setError(true)
@@ -15,7 +19,7 @@ console.log(cuisineImg);
         <tr className="relative" key={name + price + availableAt}>
             {!show && <div className=" absolute h-full w-full bg-red-500 bg-opacity-80 backdrop-blur-2xl flex items-center justify-center" > This item is not available, please delete it from your cart. </div>}
             <td className="msm:px-2 px-6 py-4 whitespace-nowrap text-sm font-medium  ">
-                <img onError={imgError} loading="lazy" className="rounded-md " src={cuisineImg} width={150} height={150} alt="cuisineImg" />
+                <img onError={imgError} loading="lazy" className="rounded-md " src={(params.checkoutType === "cartDataOnly" ? "" : "/foods/") + cuisineImg} width={150} height={150} alt="cuisineImg" />
             </td>
             <td className={`msm:px-2 px-6 py-4 whitespace-nowrap text-sm font-medium ${name?.length > 10 && "font-thin text-xs"}`}>{name}</td>
             <td className="msm:hidden msm:px-2 px-6 py-4 whitespace-nowrap text-sm ">{availableAt}</td>
